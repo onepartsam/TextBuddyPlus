8be08caf88b5180372972e5aae5b9992f60ead8c
slyj91 <slyj91@gmail.com>
2014-09-15 19:17:31 +0800

    [Unit Testing] sortTexts() - Advance
    Input:
    
    add Samuel Lim Yi Jie
    add Muzamir
    add Candiie Bay Chuan Wei
    add Lin Chen-Hsin
    
    Expected Output:
    
    1. Candiie Bay Chuan Wei
    2. Lin Chen-Hsin
    3. Muzamir
    4. Samuel Lim Yi Jie

--- src/UnitTest.java
+++ src/UnitTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class UnitTest {
 
 	@Test
-	public void testSort() {
+	public void testSortBasic() {
 		
 		TextBuddy textBuddy = new TextBuddy(new String[0]);
 		
@@ -19,6 +19,27 @@ public class UnitTest {
 		LinkedList<String> test = new LinkedList<String>();
 		test.add("A");
 		test.add("B");
+		
+		assertEquals(test, textBuddy.getTexts());
+	}
+	
+	@Test
+	public void testSortAdvance() {
+		
+		TextBuddy textBuddy = new TextBuddy(new String[0]);
+		
+		textBuddy.addText("Samuel Lim Yi Jie");
+		textBuddy.addText("Muzamir");
+		textBuddy.addText("Candiie Bay Chuan Wei");
+		textBuddy.addText("Lin Chen-Hsin");
+		
+		textBuddy.sortTexts();
+		
+		LinkedList<String> test = new LinkedList<String>();
+		test.add("Candiie Bay Chuan Wei");
+		test.add("Lin Chen-Hsin");
+		test.add("Muzamir");
+		test.add("Samuel Lim Yi Jie");
 		
 		assertEquals(test, textBuddy.getTexts());
 	}
